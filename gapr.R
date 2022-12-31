e4e35be9f80e6a62fa199f26970e8f7c3d0a8e2c
Syed Baryalay <syed@Syeds-MacBook-Pro.local>
2022-12-31 15:45:10 +0000

    bug fixed in summarise function

--- gapr.R
+++ gapr.R
@@ -3,27 +3,25 @@ library(dplyr)
 library(tidyr)
 library(gapminder)
 
-
 # summarize (...)
 # plot (...)
 # fit ()
 # predict()
 
-summarize <- function(x,
-                      filter_var,
-                      filter_value,
-                      group_var,
+summarize <- function(x, filter_var, filter_value, group_var,
                       filter_func = (`==`),
-                      summary_func = median) {
+                      summary_func = list(min=min, max=max)) {
     x |>
         dplyr::filter({{ filter_var }} |> filter_func(filter_value)) |>
         dplyr::group_by({{ group_var }}) |>
-        dplyr::select(year:gdpPercap) |>
-        dplyr::summarize_all(.tbl=_, summary_func) 
+        dplyr::select(where(is.numeric) & -year) |>
+        dplyr::summarise_all(.tbl=_, summary_func) 
 }
 
 s <- summarize(gapminder,
-          continent, "Europe",
-          year,
-          filter_func = (`==`),
-          summary_func = mean); s
+          pop, 10000000,
+          year, 
+          filter_func = (`>=`)); s
+
+
+
